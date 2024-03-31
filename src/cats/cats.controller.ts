a9c19a2d805ff4a949fa1789410858da75bc99ec
jesus jimenez <jesus@jesuss-MBP.Home>
2024-03-31 18:55:21 +0100

    cats service

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,30 +1,19 @@
 import { Controller, Get, Query, Post, Body, Put, Param, Delete } from '@nestjs/common';
-import { CatDto, ListAllEntities } from './dto';
+import { CatDto } from './dto';
+import { CatsService } from './cats.service';
+import { Cat } from './cats.interface';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
+
   @Post()
-  create(@Body() createCatDto: CatDto) {
-    return 'This action adds a new cat';
+  async create(@Body() createCatDto: CatDto) {
+    this.catsService.create(createCatDto)
   }
 
   @Get()
-  findAll(@Query() query: ListAllEntities) {
-    return `This action returns all cats (limit: ${query.limit} items)`;
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return `This action returns a #${id} cat`;
-  }
-
-  @Put(':id')
-  update(@Param('id') id: string, @Body() updateCatDto: CatDto) {
-    return `This action updates a #${id} cat`;
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return `This action removes a #${id} cat`;
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll()
   }
 }
